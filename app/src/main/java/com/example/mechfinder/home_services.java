7f01ed66e53b4a10610854364ce0b2eeac0be992
ajmalpk23 <61465738+ajmalpk23@users.noreply.github.com+ajmalpk23@gmail.com>
2020-12-08 13:41:17 +0530

    android initial commit

---
+++ app/src/main/java/com/example/mechfinder/home_services.java
@@ -0,0 +1,14 @@
+package com.example.mechfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home_services extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_services);
+    }
+}
